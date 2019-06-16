b45dd57f3e43d6eb5aba0853a30cbc43d5f4a280
Adolfo García (Dev) <adolfogh@outlook.com>
2019-06-16 17:09:57 +0200

    Añadida búsqueda al listado

--- OdeToFood.Data/IRestaurantData.cs
+++ OdeToFood.Data/IRestaurantData.cs
@@ -9,7 +9,7 @@ namespace OdeToFood.Data
 {
     public interface IRestaurantData
     {
-        IEnumerable<Restaurant> GetAll();
+        IEnumerable<Restaurant> GetRestaurantsByName(string name);
     }
 
     public class InMemoryRestaurantData : IRestaurantData
@@ -20,15 +20,16 @@ namespace OdeToFood.Data
         {
             restaurants = new List<Restaurant>()
             {
-                new Restaurant {Id=1, Name="Scott's Pizza",   Location="Maryland", Cuisine=CuisineType.None},
+                new Restaurant {Id=1, Name="Scott's Pizza",   Location="Maryland", Cuisine=CuisineType.Italian},
                 new Restaurant {Id=1, Name="Cinnamon Club",   Location="London", Cuisine=CuisineType.Indian},
                 new Restaurant {Id=1, Name="La Costa",   Location="California", Cuisine=CuisineType.Mexican}
             };
         }
 
-        public IEnumerable<Restaurant> GetAll()
+        public IEnumerable<Restaurant> GetRestaurantsByName( string name = null)
         {
             return from r in restaurants
+                   where string.IsNullOrEmpty(name) || r.Name.StartsWith(name)
                    orderby r.Name
                    select r;
         }
